e00e177832c19ca93ca078a3931d1e6b20bc0aae
wonseok_vivident <wslee@vivident.xyz>
2026-01-09 18:21:09 +0900

    데이터 정제 코드

--- storyGenerator/fullStoryGenerator.js
+++ storyGenerator/fullStoryGenerator.js
@@ -1,4 +1,4 @@
-function storyGenerate() {
+function fullStoryGenerate() {
     const endPoint = "https://data-generate-api-570233004501.asia-northeast3.run.app/ai-create/full-story-generate";
     const promptFile = "story_generate_fullScript.txt";
 
@@ -6,17 +6,19 @@ function storyGenerate() {
     var masterFile = SpreadsheetApp.openById(fileId);
     var sheetid = SpreadsheetApp.getActive().getId();
 
-    var values = SpreadsheetApp.getActive().getSheetByName("dialog_generator").getDataRange().getValues();
+    var values = SpreadsheetApp.getActive().getSheetByName("script_info").getDataRange().getValues();
     var inputHeader = values[0];
     var inputData = values.slice(1);
     var inputH = makeHeaderIndex_(inputHeader);
 
+    //용어집 데이터화
     const dictionaryId = PropertiesService.getScriptProperties().getProperty("DICTIONARY");
     const dictionarySheet = SpreadsheetApp.openById(dictionaryId).getSheetByName("Dictionary");
     const dicValues = dictionarySheet.getDataRange().getValues();
     const dicObj = convertToObject_(dicValues);
     const dictionary = arrayToDictionary(dicObj, "ko-KR");
 
+    //캐릭터 참조 테이블 매핑 구성
     const speakerSheet = SpreadsheetApp.getActive().getSheetByName("dialogSpeaker");
     const speakerValues = speakerSheet.getDataRange().getValues();
     const speakerHeader = speakerValues[0];
@@ -28,67 +30,59 @@ function storyGenerate() {
         ["Name"],
         "id"
     );
-    const modelIndex = buildLookupCompositeOne(
-        speakerData,
-        speakerHeader,
-        ["id"],
-        "model"
+
+    //게임 시스템 타입 참조 테이블 매핑 구성
+    const systemSheetData = SpreadsheetApp.getActive().getSheetByName("ref_system").getDataRange().getValues();
+    const systemDataHeader = systemSheetData[0];
+    const systemDataBody = systemSheetData.slice(1);
+
+    const systemIndex = buildLookupCompositeOne(
+        systemDataBody,
+        systemDataHeader,
+        ["Name"],
+        "id"
     );
 
-    const sceneInfoSheet = SpreadsheetApp.getActive().getSheetByName("scene_info");
-    const sceneInfoValues = sceneInfoSheet.getDataRange().getValues();
-    const sceneInfoHeader = sceneInfoValues[0];
-    const sceneInfoData = sceneInfoValues.slice(1);
-    const sceneInfoH = makeHeaderIndex_(sceneInfoHeader);
-    const selectedComlumnsSceneInfo = [
-        sceneInfoH["sceneId"],
-        sceneInfoH["narrationTone"],
-        sceneInfoH["writingStyle"],
-        sceneInfoH["introContext"],
-        sceneInfoH["location"]
-    ];
-    const sceneHeader = [
-      "sceneId",
-      "narrationTone",
-      "writingStyle",
-      "introContext",
-      "location"
-    ];
-    const sceneIds = sceneInfoData
-        .filter((row) => String(row[sceneInfoH["isGenerate"]]).trim() === "true")
-        .map((row) => row[sceneInfoH["sceneId"]]);
+    const selectedColumn = [
+        inputH["sceneId"],
+        inputH["character"],
+        inputH["systemKind"],
+        inputH["DirectionWithContext"],
+        inputH["location"],
+        inputH["model"],
+        inputH["temperature"]
+    ]
+    const sceneIds = values
+        .filter((row) => String(row[inputH["isGenerate"]]).trim() === "true")
+        .map((row) => row[inputH["sceneId"]]);
 
-    const sceneInfoFiltered = sceneInfoData
-        .filter((row) => String(row[sceneInfoH["isGenerate"]]).trim() === "true")
-        .map((row) => selectedComlumnsSceneInfo.map((colIdx) => row[colIdx]) || "");
-
-    sceneInfoFiltered.unshift(sceneHeader);
-    const sceneInfoConvert = convertToObject_(sceneInfoFiltered);
-    const sceneInfoObj = arrayToDictionary(sceneInfoConvert, "sceneId");
-
+    
+    //마스터 테이블에서 필요한 데이터들 인덱스 매핑 구성
     const refData = loadRefData(masterFile);
-
     const enumIndex = buildLookupCompositeOne(
       refData["enum"].data,
       refData["enum"].header,
       ["#Name"],
       "Name"
     );
-
     const spaceTitleIndex = buildLookupCompositeOne(
         refData["spaces"].data,
         refData["spaces"].header,
         ["#Name"],
         "title"
     );
-
     const spaceDescIndex = buildLookupCompositeOne(
         refData["spaces"].data,
         refData["spaces"].header,
         ["#Name"],
         "description"
     );
-
+    const spaceParentIndex = buildLookupCompositeOne(
+        refData["spaces"].data,
+        refData["spaces"].header,
+        ["#Name"],
+        "parentId"
+    );
     const localizationIndex = buildLookupCompositeOne(
       refData["localization"].data,
       refData["localization"].header,
@@ -98,106 +92,101 @@ function storyGenerate() {
     
     const resultHeader = [
       "sceneId",
-      "key",
-      "speaker",
-      "emotion",
-      "level",
+      "character",
+      "systemKind",
       "direction",
+      "place",
       "location",
-      "innerThought",
-      "narrationTone",
-      "writingStyle",
-      "introContext",
       "model",
       "temperature"
     ];
     const resultH = makeHeaderIndex_(resultHeader);
     const validSceneIdSet = new Set(sceneIds);
-    const filteredDialogs = inputData.filter((row) => {
-      const rowSceneId = row[inputH["sceneId"]];
+    const filteredData = inputData.filter((row) => {
+        const rowSceneId = row[inputH["sceneId"]];
 
-      return validSceneIdSet.has(rowSceneId);
+        return validSceneIdSet.has(rowSceneId);
     });
 
+    Logger.log(filteredData);
+
     const resultData = [];
-    for( const [rowIndex, rowData] of filteredDialogs.entries()) {
-        const inputSpeaker = rowData[inputH["speaker"]];
-        const speaker = inputSpeaker
-            ? lookupCompositeOne(speakerIndex, {"Name" : inputSpeaker})
+    for( const [rowIndex, rowData] of filteredData.entries() ) {
+        const inputCharacter = rowData[inputH["character"]];
+        const character = inputCharacter
+            ? lookupCompositeOne(speakerIndex, {"Name" : inputCharacter})
             : "";
-        const inputEmotion = rowData[inputH["emotion"]];
-        const emotion = inputEmotion
-            ? lookupCompositeOne(enumIndex, {"#Name" : inputEmotion})
-            : "";
-        const model = speaker
-            ? lookupCompositeOne(modelIndex, {"id" : speaker})
+        
+        const inputSystem = rowData[inputH["systemKind"]];
+        const system = inputSystem
+            ? lookupCompositeOne(systemIndex, {"Name" : inputSystem})
             : "";
 
-        var inputInnerThought = rowData[inputH["innerThought"]];
-        var innerThoughtObj = {};
-        if(inputInnerThought.toString().trim() !== "") {
-            innerThoughtObj = convertInnerThoughtToObj_(inputInnerThought, speaker);
-        }
-
-        let inputLocation = rowData[inputH["location"]];
-        let defaultLocation = sceneInfoObj[rowData[inputH["sceneId"]]]?.location || "";
-        let selectedLocation = "";
-        if(inputLocation === "") {
-          selectedLocation = defaultLocation;
-        } else {
-          selectedLocation = inputLocation;
-        }
+        const inputLocation = rowData[inputH["location"]];
+        const placeId = inputLocation
+            ? lookupCompositeOne(spaceParentIndex, {"#Name" : inputLocation})
+            : "";
         
         
-        const space_title = selectedLocation
-            ? lookupCompositeOne(spaceTitleIndex, {"#Name" : selectedLocation})
+        const locationTitleKey = inputLocation
+            ? lookupCompositeOne(spaceTitleIndex, {"#Name" : inputLocation})
             : "";
-        const space_description = selectedLocation
-            ? lookupCompositeOne(spaceDescIndex, {"#Name" : selectedLocation})
+        const locationDescriptionKey = inputLocation
+            ? lookupCompositeOne(spaceDescIndex, {"#Name" : inputLocation})
+            : "";
+        const locationKeyList = [locationTitleKey, locationDescriptionKey];
+        let location = "";
+        for(const key of locationKeyList) {
+            const text = key 
+                ? lookupCompositeOne(localizationIndex, {"key" : key})
+                : "";
+            location += (text + "\n");
+        }
+
+        const placeTitleKey = placeId
+            ? lookupCompositeOne(spaceTitleIndex, {"#Name" : placeId})
+            : "";
+        const placeDescriptionKey = placeId
+            ? lookupCompositeOne(spaceDescIndex, {"#Name" : placeId})
             : "";
 
-        const locationTitle = space_title
-            ? lookupCompositeOne(localizationIndex, {"key" : space_title})
-            : "";
-        const locationDesc = space_description
-            ? lookupCompositeOne(localizationIndex, {"key" : space_description})
-            : "";
-
-        const location = locationTitle + "\n" + locationDesc;
+        const placeKeyList = [placeTitleKey, placeDescriptionKey];
+        let place = "";
+        for(const key of placeKeyList) {
+            const text = key
+                ? lookupCompositeOne(localizationIndex, {"key" : key})
+                : "";
+            
+                plcae += (text + "\n");
+        }
+        
         
         const dataRow = new Array(resultHeader.length).fill("");
-
         dataRow[resultH["sceneId"]] = rowData[inputH["sceneId"]];
-        dataRow[resultH["key"]] = rowData[inputH["key"]];
-        dataRow[resultH["speaker"]] = speaker;
-        dataRow[resultH["emotion"]] = emotion;
-        dataRow[resultH["level"]] = rowData[inputH["level"]];
+        dataRow[resultH["character"]] = character;
+        dataRow[resultH["systemKind"]] = system;
         dataRow[resultH["direction"]] = rowData[inputH["direction"]];
+        dataRow[resultH["place"]] = place;
         dataRow[resultH["location"]] = location;
-        dataRow[resultH["innerThought"]] = innerThoughtObj;
-        dataRow[resultH["narrationTone"]] = sceneInfoObj[rowData[inputH["sceneId"]]]?.narrationTone || "";
-        dataRow[resultH["writingStyle"]] = sceneInfoObj[rowData[inputH["sceneId"]]]?.writingStyle || "";
-        dataRow[resultH["introContext"]] = sceneInfoObj[rowData[inputH["sceneId"]]]?.introContext || "";
-        dataRow[resultH["model"]] = model;
+        dataRow[resultH["model"]] = rowData[inputH["model"]];
         dataRow[resultH["temperature"]] = rowData[inputH["temperature"]];
-
+       
         resultData.push(dataRow);
-
     }
-
     resultData.unshift(resultHeader);
 
     
     const payload = {
         data : resultData,
         dictionary : dictionary,
-        sheetName : "dialog_generator",
+        sheetName : "script_generator",
         sheetId : sheetid,
         promptFile : promptFile
     };
 
     Logger.log(JSON.stringify(payload.data, null, 2));
 
+    /*
     const options = {
         method : "post",
         contentType : "application/json",
@@ -212,4 +201,5 @@ function storyGenerate() {
     } catch (e) {
         Logger.log(`Error sending : ${e.message}`);
     }
+    */
 }
